3f14717c7543398e280d3f3e5981a0e1c84fb06a
N-Rain <bngoc1792@gmail.com>
2023-10-08 19:51:24 +0700

    week 4_ useState

---
+++ week_4/App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Page_2a from './views/Page_2a.js'
+export default function App() {
+  return (
+    <Page_2a/>
+)};
